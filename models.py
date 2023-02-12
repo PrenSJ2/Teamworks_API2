73dff426aecf3f1b6efbced1b9aed08bab7f9dfa
PrenSJ2 <seb.prentice@hotmail.com>
2023-02-12 19:18:55 +0000

    updated for sqlalchemy

--- models.py
+++ models.py
@@ -1,54 +1,67 @@
-from sqlalchemy import Column, DATETIME, Integer, Float, String, Boolean, Text, ForeignKey
+from typing import List
+from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'users'
+    UID = Column(Integer, primary_key=True)
+    DateofBirth = Column(DateTime)
+    First_name = Column(String)
+    Last_name = Column(String)
+    Email = Column(String, unique=True)
+    Password = Column(String)
+    Phone_number = Column(String)
+    isHost = Column(Boolean)
 
-class User(BaseModel):
-    UID: int
-    DateofBirth: str
-    First_name: str
-    Last_name: str
-    Email: str
-    Password: str
-    Phone_number: str
-    isHost: bool
+class Office(Base):
+    __tablename__ = 'offices'
+    OID = Column(Integer, primary_key=True)
+    Name = Column(String)
+    Location = Column(String)
+    NumGuests = Column(String)
+    Sqft = Column(String)
+    Description = Column(String)
+    Hourly = Column(Float)
+    MinHours = Column(String)
+    Amenities = Column(String)
+    Features = Column(String)
+    Image = Column(String)
+    UID = Column(Integer)
 
-class Office(BaseModel):
-    OID: int
-    Name: str
-    Location: str
-    NumGuests: str
-    Sqft: str
-    Description: str
-    Hourly: float
-    MinHours: str
-    Amenities: str
-    Features: str
-    Image: str
-    UID: int
+class Booking(Base):
+    __tablename__ = 'bookings'
+    BID = Column(Integer, primary_key=True)
+    Start_datetime = Column(DateTime)
+    End_datetime = Column(DateTime)
+    isActive = Column(Boolean)
+    Total_hours = Column(Float)
+    NumGuests = Column(Integer)
+    Activity = Column(String)
+    TimePeriod = Column(String)
+    OID = Column(Integer)
+    HID = Column(Integer)
+    OName = Column(String)
+    OLocation = Column(String)
+    UID = Column(Integer)
+    GName = Column(String)
+    OfficePrice = Column(Float)
+    ServiceFee = Column(Float)
+    FinalPrice = Column(Float)
 
-class Booking(BaseModel):
-    BID: int
-    Start_datetime: str
-    End_datetime: str
-    isActive: bool
-    Total_hours: float
-    NumGuests: int
-    Activity: str
-    TimePeriod: str
-    OID: int
-    HID: int
-    OName: str
-    OLocation: str
-    UID: int
-    GName: str
-    OfficePrice: float
-    ServiceFee: float
-    FinalPrice: float
+class Addon(Base):
+    __tablename__ = 'addons'
+    AID = Column(Integer, primary_key=True)
+    Name = Column(String, unique=True)
+    Quantity = Column(Integer)
+    Price = Column(Float)
+    ImgName = Column(String)
+    OID = Column(Integer)
 
-class Addon(BaseModel):
-    AID: int
-    Name: str
-    Quantity: int
-    Price: float
-    ImgName: str
-    OID: int
+engine = create_engine('sqlite:///data.db')
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
